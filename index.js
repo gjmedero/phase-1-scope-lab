b572afea92a7948c840e8a7381b794a8d9735791
Gilberto Medero <gjmedero@gmail.com>
2023-03-24 16:01:51 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your solution in this file!
+var customerName = "bob";
+var bestCustomer;
+const LeastFavoriteCustomer = "purple"
+
+function upperCaseCustomerName() {
+    customerName = "BOB";
+}
+
+function setBestCustomer() {
+    bestCustomer = "not bob";
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = "maybe bob";
+}
+
+function changeLeastFavoriteCustomer() {
+    LeastFavoriteCustomer = "blue";
+}
